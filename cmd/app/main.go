c5355423c71526a3a6f73db80a496b74673d0955
DurininKirous <blackmetalchel@mail.ru>
2025-10-17 17:23:17 +0300

    statistics + argument supporting

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,15 +1,38 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"goping/internal/flags"
 	"goping/internal/ping"
+	"os"
 	"time"
 )
 
 func main() {
 	flags.InitFlags()
+
+	var sent, received int
+	var rtts []time.Duration
+
+	if len(flag.Args()) != 1 {
+		fmt.Println("Usage: goping [options] <address>")
+		os.Exit(1)
+	}
+
 	for i := 0; i != flags.Count; i++ {
-		ping.PingOnce("0.0.0.0", "HUI", "8.8.8.8")
+		sent++
+		start := time.Now()
+		ok := ping.PingOnce("0.0.0.0", "HUI", flag.Args()[0])
+		if ok {
+			received++
+			rtts = append(rtts, time.Since(start))
+		}
 		time.Sleep(time.Duration(flags.Interval * float64(time.Second)))
 	}
+
+	loss := float64(sent-received) / float64(sent) * 100
+	fmt.Printf("\n--- %s ping statistics ---\n", flag.Args()[0])
+	fmt.Printf("%d packets transmitted, %d received, %.1f%% packet loss\n", sent, received, loss)
+
 }
--- internal/ping/ping.go
+++ internal/ping/ping.go
@@ -1,7 +1,9 @@
 package ping
 
 import (
+	"fmt"
 	"log"
+	"os"
 
 	"goping/internal/icmp/reply"
 	"goping/internal/icmp/request"
@@ -9,16 +11,27 @@ import (
 	"golang.org/x/net/ipv4"
 )
 
-func PingOnce(listenAddress string, messageSend string, sendToAddress string) {
+func PingOnce(listenAddress string, messageSend string, sendToAddress string) bool {
 	socket := request.SocketInit(listenAddress, nil)
 	messageSendPayload := request.MessageInit(messageSend)
 	messageSendPayloadBytes := request.MessageToBytes(messageSendPayload)
 	start := request.SendIcmpPacketBytes(socket, messageSendPayloadBytes, sendToAddress)
-	messageGetPayload, ipHeader, n, peer, rtt := reply.GetMessage(socket, start)
+	messageGetPayload, ipHeader, n, peer, rtt, err := reply.GetMessage(socket, start)
+	if err != nil {
+		if os.IsTimeout(err) {
+			fmt.Println("Request timed out")
+		} else {
+			log.Fatal(err)
+		}
+		socket.Close()
+		return false
+	}
 	switch messageGetPayload.Type {
 	case ipv4.ICMPTypeEchoReply:
 		log.Printf("got reflection from %v, len %d, ttl %d, time %.1f ms", peer, n, ipHeader.TTL, float64(rtt.Microseconds())/1000)
+		return true
 	default:
 		log.Printf("got %+v; want echo reply", messageGetPayload)
+		return false
 	}
 }
